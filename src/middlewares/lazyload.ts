24c063485f5414a5d9832618ae0863c02bf85b38
Necati <necatifk07@gmail.com>
2022-08-11 22:37:07 +0300

    add lazyload

---
+++ src/middlewares/lazyload.ts
@@ -0,0 +1,39 @@
+import { Middleware, Slider } from '~/types';
+import { getElements } from '~/utils/getElement';
+
+const loadImage = (img: HTMLImageElement) => {
+  const src = img.getAttribute('data-src');
+
+  if (!src) return;
+
+  img.removeAttribute('data-src');
+  img.src = src;
+};
+
+const loadImages = (newSlider: Slider) => {
+  const slides = newSlider.slides
+    .slice(
+      newSlider.activeView, newSlider.activeView + newSlider.slidesPerView,
+    );
+
+  const images = slides.flatMap(
+    (slide) => getElements<HTMLImageElement>(slide.querySelectorAll('img')),
+  );
+
+  images.forEach(loadImage);
+};
+
+const lazyload = (): Middleware => ({
+  name: 'lazyload',
+  callback: (slider) => {
+    const onSlideChange = (newSlider: Slider) => {
+      loadImages(newSlider);
+      slider.removeSlideChangeHook(onSlideChange);
+    };
+
+    slider.onSlideChange(onSlideChange);
+    slider.onCleanUp(() => slider.removeSlideChangeHook(onSlideChange));
+  },
+});
+
+export default lazyload;
--- src/utils/getElement.ts
+++ src/utils/getElement.ts
@@ -1,5 +1,5 @@
-export type Selector = string | HTMLElement | null;
-export type SelectorMultiple = string | HTMLElement[] | NodeList | null;
+export type Selector<T = HTMLElement> = string | T | null;
+export type SelectorMultiple<T = HTMLElement> = string | T[] | NodeList | null;
 
 const getElement = (selector?: Selector, fallback?: () => Selector): HTMLElement | null => {
   if (!selector) {
@@ -12,17 +12,17 @@ const getElement = (selector?: Selector, fallback?: () => Selector): HTMLElement
   return getElement(document.querySelector(selector) as HTMLElement);
 };
 
-const getElements = (
-  selector?: SelectorMultiple,
-  fallback?: () => SelectorMultiple,
-): HTMLElement[] => {
+const getElements = <T = HTMLElement>(
+  selector?: SelectorMultiple<T>,
+  fallback?: () => SelectorMultiple<T>,
+): T[] => {
   if (!selector) {
-    if (fallback) return getElements(fallback());
+    if (fallback) return getElements<T>(fallback());
     return [];
   }
 
   if (selector instanceof NodeList) {
-    return [...selector] as HTMLElement[];
+    return [...selector] as T[];
   }
 
   if (Array.isArray(selector)) return selector;
